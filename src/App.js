c18a339b5e2cffab9d9f332ed3cb209e8b8f3095
Gabriel <gabrielulu@gmail.com>
2018-01-05 13:25:23 +0800

    added menu

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
           <ul className='nav'>
             {/* <Scrollchor to="#sample-code" className="nav-link">Sample</Scrollchor> */}
             <li><Link to='/'> Home </Link></li>
-            <li><Scrollchor to='#sample-code'>Locations</Scrollchor></li>
-            <li><Link to='/menu'> Menu </Link></li>
+            <li><Scrollchor to='#locations'>Locations</Scrollchor></li>
+            <li><Scrollchor to='#menu'>Menu</Scrollchor></li>
             <li><Link to='/menu'> Reservations </Link></li>
             {/* <li><Link to='/'> Home </Link></li>
             <li><Link to='/locations'> Locations </Link></li>
@@ -22,7 +22,7 @@ class App extends Component {
           <img src='image/chickbannertransparent.png' />
         </div>
         <Locations />
-
+        <Menu />
         {/* <Route exact={true} path='/' component={Home} />
         <Route path='/locations' component={Locations} /> */}
       </div>
@@ -33,7 +33,7 @@ class App extends Component {
 class Locations extends Component {
   render () {
     return (
-      <div className='componentSize'> <h1 id='sample-code'> Locations </h1>
+      <div className='componentSize'> <h1 id='locations'> Locations </h1>
         <img src='https://maps.googleapis.com/maps/api/staticmap?center=8+Claymore+Hill,Singapore,SG&zoom=16&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:G%7C1.308089,103.831722&key=AIzaSyAy0PNSC9SbRpl9b31EtW9HCdcsDBD65IA' />
         <div>
           <p> Contact: +65 1234567, gabrielulu@gmail.com <br />
@@ -41,6 +41,22 @@ class Locations extends Component {
 
           </p>
         </div>
+      </div>
+    )
+  }
+}
+
+class Menu extends Component {
+  render () {
+    return (
+      <div> <h1> Menu </h1>
+        <img id='menu' src='image/menu.png' />
+        {/* <div>
+          <p> Contact: +65 1234567, gabrielulu@gmail.com <br />
+              Address: 8 Claymore Hill, Singapore 229572 <br />
+
+          </p>
+        </div> */}
       </div>
     )
   }
